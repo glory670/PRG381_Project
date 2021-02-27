7d08d74c020ec66bcbbdb479861ef6424c7f3e39
Mar-debug <72023670+Mar-debug@users.noreply.github.com>
2021-02-27 20:41:13 +0200

    Added get and set

--- DataAccessLayer/Address.java
+++ DataAccessLayer/Address.java
@@ -8,6 +8,56 @@ public class Address {
     String City;
     String PhoneNo;
 
+    public Integer getAddressID()
+    {
+        return AddressID;
+    }
+    public void setAddressID(Integer AddressID)
+    {
+        this.AddressID = AddressID;
+    }
+    public Integer getUnitNo()
+    {
+        return UnitNo;
+    }
+    public void setUnitNo(Integer UnitNo)
+    {
+        this.UnitNo = UnitNo;
+    }
+    public String getStreet()
+    {
+        return Street;
+    }
+    public void setStreet(String Street)
+    {
+        this.Street = Street;
+    }
+    public String getSuburb()
+    {
+        return Suburb;
+    }
+    public void setSuburb(String Suburb)
+    {
+        this.Suburb = Suburb;
+    }
+    public String getCity()
+    {
+        return City;
+    }
+    public void setCity(String City)
+    {
+        this.City = City;
+    }
+    public String getPhoneNo()
+    {
+        return PhoneNo;
+    }
+    public void setPhoneNo(String PhoneNo)
+    {
+        this.PhoneNo = PhoneNo;
+    }
+
+    
     public Address()
     {
 
