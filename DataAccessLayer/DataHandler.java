70e25824ed228a301a6ef7205d5c783e5bb9aa5d
Phumelele-Mthimunye <mrgeminiaura@gmail.com>
2021-02-28 13:37:44 +0200

    Client Login method

--- DataAccessLayer/DataHandler.java
+++ DataAccessLayer/DataHandler.java
@@ -2,16 +2,20 @@ package DataAccessLayer;
 import BusinessLogicLayer.*;
 import java.io.*;
 import java.util.*;
+
 public class DataHandler {
+    String filepath = "C:\\Users\\Aura\\Desktop\\PRG381 Project\\Clients.txt";
     public List<Clients> GetClients() throws FileNotFoundException{
         List<Clients> clist = new ArrayList<Clients>();
-        File file = new File("C:\\Users\\Aura\\Desktop\\PRG381 Project\\Clients.txt");
+        
+        File file = new File(filepath);
         Scanner sc = new Scanner(file);
         while(sc.hasNextLine()){
             String[] line = sc.nextLine().split(",");
             clist.add(new Clients(Integer.parseInt(line[0]),line[1],line[2],Integer.parseInt(line[3]),line[4],line[5],line[6]));
 
         }
+        sc.close();
         return clist;
     }
 
@@ -31,6 +35,7 @@ public class DataHandler {
                     linecount++;
                 }
                 client.setClientID(linecount+1);
+                lr.close();
             }
 
             System.out.print(" Enter firstName => ");
@@ -58,6 +63,7 @@ public class DataHandler {
            client.setPassword(password);
 
            pw.println(client.toString());
+           pw.close();
 
            //System.out.println(client.toString());
         }
@@ -76,9 +82,54 @@ public class DataHandler {
         //}
         //scanner.close();
     }
-    public void verifyLogin(String email,String password){
-        
+    public static Scanner x;
+    public void verifyClientLogin(String email,String password){
+        boolean accepted = false;
+        while (!accepted){
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Enter email => ");
+            email = scan.nextLine();
 
+            System.out.print(" Enter password => ");
+            password = scan.nextLine();
+
+            scan.close();
+
+            boolean found = false;
+            String tempclientID ="";
+            String tempname="";
+            String tempsurname="";
+            int tempage = 0;
+            String tempEmail = "";
+            String tempPhoneNo="";
+            String tempPassword = "";
+
+            try {
+                x = new Scanner(new File(filepath));
+                x.useDelimiter("[,\n]");
+                while(x.hasNext()&& !found){
+                    tempclientID = x.next();
+                    tempname = x.next();
+                    tempsurname = x.next();
+                    tempage = Integer.parseInt(x.next());
+                    tempEmail = x.next();
+                    tempPhoneNo = x.next();
+                    tempPassword = x.next();
+
+                    if(tempEmail.trim().equals(email.trim()) && tempPassword.trim().equals(password)){
+                        found = true;
+                        accepted = true;
+                    }
+                }
+                x.close();
+                System.out.println("User is Found: Welcome "+ tempname +" "+tempsurname);
+                
+            } catch (Exception e) {
+                //TODO: handle exception
+                System.out.println("Error:User not Found");
+            }
+        }
+        
     }
 
 }
